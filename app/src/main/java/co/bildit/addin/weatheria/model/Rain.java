30294069703754b94aa8274a8cd96fb675a3693b
Addin Gama Bertaqwa <>
2022-03-28 16:08:27 +0800

    Add generated model from jsonschema2pojo generator

---
+++ app/src/main/java/co/bildit/addin/weatheria/model/Rain.java
@@ -0,0 +1,22 @@
+package co.bildit.addin.weatheria.model;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Rain {
+
+@SerializedName("3h")
+@Expose
+private Double _3h;
+
+public Double get3h() {
+return _3h;
+}
+
+public void set3h(Double _3h) {
+this._3h = _3h;
+}
+
+}
